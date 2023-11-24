31a18d11467423a57a58fc004d7384a02cd60966
V <116948898+raazrifany@users.noreply.github.com>
2023-11-24 23:25:09 +0700

    Add files via upload

---
+++ p1_latihan.php
@@ -0,0 +1,17 @@
+<html>
+    <head><title>Latihan biodata</title></head>
+    <body>
+        <?php
+            echo"<h2 align='center'>Company Profile</h2>";
+            echo"<div id='foto' align='center' style='margin: auto;'>";
+                echo"<img src='ijong.jpeg' width='200' height='240'/>";
+            echo"</div><br>";
+            echo"<div id='teks' style='width:250px; margin: auto;'>";
+                echo"<p>Nama: Lee Jeonghyeon</p>";
+                echo"<p>Alamat: Seoul</p>";
+                echo"<p>Hobi: Solve a rubic cube</p>";
+                echo"<p>Moto: Just live ur best life</p>";
+            echo"</div>";
+        ?>
+    </body>
+</html>
